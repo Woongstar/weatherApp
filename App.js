3f0e1d195db3f6833ad9bc319fb0e51e65504826
Jiwoong Yang <qualtypo@gmail.com>
2021-04-07 23:27:50 +0900

    Add getLocation

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import Loading from "./Loading";
+import * as Location from "expo-location";
+import { Alert } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class extends React.Component {
+  state = {
+    inLoading: true,
+  };
+  getLocation = async () => {
+    try {
+      await Location.requestPermissionsAsync();
+      const {
+        coords: { latitude, longtitude },
+      } = await Location.requestPermissionsAsync();
+      // Send to API and get weather
+      this.setState({ isLoading: false });
+    } catch (error) {
+      Alert.alert("Can't find you", "So sad");
+    }
+  };
+  conponentDidMount() {
+    this.getLocation();
+  }
+  render() {
+    const { isLoading } = this.state;
+    return inLoading ? <Loading /> : null;
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
