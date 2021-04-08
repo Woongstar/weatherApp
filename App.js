a6a6056b3494ef4aaf89ca1a6c79c1cfad70c0ca
Jiwoong Yang <qualtypo@gmail.com>
2021-04-08 14:10:48 +0900

    add local weather

--- App.js
+++ App.js
@@ -2,11 +2,21 @@ import React from "react";
 import Loading from "./Loading";
 import * as Location from "expo-location";
 import { Alert } from "react-native";
+import axios from "axios";
+
+const APIkey = "ed17ca8cd7259aa5f225fa92a3579ad3";
 
 export default class extends React.Component {
   state = {
     inLoading: true,
   };
+  getWeather = async (latitude, longtitude) => {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longtitude}&APPID=${APIkey}`
+    );
+    console.log(data);
+  };
+
   getLocation = async () => {
     try {
       await Location.requestPermissionsAsync();
@@ -24,6 +34,6 @@ export default class extends React.Component {
   }
   render() {
     const { isLoading } = this.state;
-    return inLoading ? <Loading /> : null;
+    return isLoading ? <Loading /> : null;
   }
 }
